227cef7d21d2ecb4f33498b137fc2cbe331e1982
p0nk0 <Roberts.taylor000@gmail.com>
2024-03-14 16:19:01 -0400

    nums

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -11,6 +11,43 @@ export default function QueryProcessor(query: string): string {
       "Taylor"
     );
   }
+  else if (query.toLowerCase().includes("largest")) {
+    let nums = query.slice(query.indexOf(":") + 1, query.indexOf("?"));
+    let nums1 = nums.split(", ");
+    let nums2 = nums1.map(item => parseInt(item.trim()));
+    console.log(nums);
+    console.log(nums1);
+    console.log(nums2);
+    return (
+      Math.max(...nums2).toString()
+    );
+  }
+  else if (query.toLowerCase().includes("plus")) {
+    let nums = query.slice(query.indexOf("is") + 2, query.indexOf("?"));
+    let nums1 = nums.split(" plus ");
+    let nums2 = nums1.map(item => parseInt(item.trim()));
+    console.log(nums);
+    console.log(nums1);
+    console.log(nums2);
+    return (
+      (nums2[0] + nums2[1]).toString()
+    );
+  }
+  else if (query.toLowerCase().includes("square and a cube")) {
+    let nums = query.slice(query.indexOf(":") + 1, query.indexOf("?"));
+    let nums1 = nums.split(" plus ");
+    let nums2 = nums1.map(item => parseInt(item.trim()));
+    let nums3 = nums2.filter(num => (Math.sqrt(num) % 1 === 0) && (Math.cbrt(num) % 1 === 0))
+    console.log(nums);
+    console.log(nums1);
+    console.log(nums2);
+    
+    return (
+      (nums3[0]).toString()
+    );
+  }
+  
+
 
   return "";
 }
